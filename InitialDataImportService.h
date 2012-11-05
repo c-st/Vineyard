1e0a30717134fe0d67fe03065efe82c3d1844b26
Christian Stangier <christian.stangier@arithnea.de>
2012-11-05 18:46:52 +0100

    set relationships manually, add InitialDataImportService

---
+++ InitialDataImportService.h
@@ -0,0 +1,13 @@
+//
+//  InitialDataImportController.h
+//  Cellar
+//
+//  Created by Christian Stangier on 05.11.12.
+//  Copyright (c) 2012 Christian Stangier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InitialDataImportController : NSObject
+
+@end
---
+++ InitialDataImportService.m
@@ -0,0 +1,13 @@
+//
+//  InitialDataImportController.m
+//  Cellar
+//
+//  Created by Christian Stangier on 05.11.12.
+//  Copyright (c) 2012 Christian Stangier. All rights reserved.
+//
+
+#import "InitialDataImportController.h"
+
+@implementation InitialDataImportController
+
+@end
