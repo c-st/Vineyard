b8d52bd3eba125d248b31db7ff145f4c8f6d7fd1
Christian Stangier <christian.stangier@arithnea.de>
2012-12-02 18:18:00 +0100

    icon reverted to black.

--- Views/CellarTableViewCell.m
+++ Views/CellarTableViewCell.m
@@ -9,7 +9,6 @@
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-		NSLog(@"CellarTableViewCell");
         [self setShowArrow:NO];
     }
     return self;
