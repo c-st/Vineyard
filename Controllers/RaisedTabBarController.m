a86c35997d971876e507b80976cdd266d5f01fd6
Christian Stangier <christian.stangier@arithnea.de>
2012-12-11 21:40:35 +0100

    added a fold navigation controller for collections

--- Controllers/RaisedTabBarController.m
+++ Controllers/RaisedTabBarController.m
@@ -7,6 +7,10 @@
 #import "VarietalTableViewController.h"
 #import "Country.h"
 
+#import "PaperFoldView.h"
+#import "PaperFoldNavigationController.h"
+
+
 @implementation RaisedTabBarController
 
 // Create a view controller and setup it's tab bar item with a title and image
@@ -59,7 +63,6 @@
 	[countryTableViewController setTitle:@"Countries"];
 	[countryTableViewController setShowCount:YES];
 	countryTableViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Countries" image:[UIImage imageNamed:@"globe-icon.png"] tag:0];
-	
     UINavigationController *countryNavController = [[UINavigationController alloc] initWithRootViewController:countryTableViewController];
 	
 	// Wines
@@ -76,19 +79,47 @@
 	varietalTableViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Varietals" image:[UIImage imageNamed:@"food_grapes.png"] tag:0];
 	UINavigationController *varietalNavController = [[UINavigationController alloc] initWithRootViewController:varietalTableViewController];
 	
-	// Collections
-	// ...
+	// -- Collections
+	
+    //[self.window setRootViewController:paperFoldNavController];
+	
+	UIViewController *collectionsController = [[UIViewController alloc] init];
+	//[collectionsController.view setFrame:self.view.frame];
+	[collectionsController setTitle:@"Collections"];
+	[collectionsController.view setBackgroundColor:[UIColor cellarBeigeColour]];
+	
+	
+	UINavigationController *collectionsNavController = [[UINavigationController alloc] initWithRootViewController:collectionsController];
+	PaperFoldNavigationController *paperFoldNavController = [[PaperFoldNavigationController alloc] initWithRootViewController:collectionsNavController];
+	paperFoldNavController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Collections" image:[UIImage imageNamed:@"collections.png"] tag:0];
+	
+	// Left view (dummy)
+	UIViewController *leftVC = [[UIViewController alloc] init];
+	[leftVC.view setFrame:CGRectMake(0,0,100,[self.view bounds].size.height)];
+	[leftVC.view setBackgroundColor:[UIColor greenColor]];
+	[paperFoldNavController setLeftViewController:leftVC width:150.0];
+	
+	// Right view
+	UIViewController *rightVC = [[UIViewController alloc] init];
+	[rightVC.view setFrame:CGRectMake(0,0,240,[self.view bounds].size.height)];
+	[rightVC.view setBackgroundColor:[UIColor blueColor]];
+	[paperFoldNavController setRightViewController:rightVC width:250.0 rightViewFoldCount:3 rightViewPullFactor:0.9];
+	
+	
+	
+	
 	
 	[wineNavController.navigationBar setTintColor:[UIColor cellarWineRedColour]];
 	[countryNavController.navigationBar setTintColor:[UIColor cellarWineRedColour]];
 	[varietalNavController.navigationBar setTintColor:[UIColor cellarWineRedColour]];
+	[collectionsNavController.navigationBar setTintColor:[UIColor cellarWineRedColour]];
 	
 	[self setViewControllers:[NSArray arrayWithObjects:
 							wineNavController,
                             countryNavController,
                             [self viewControllerWithTabTitle:@"Add" image:nil],
                             varietalNavController,
-                            [self viewControllerWithTabTitle:@"Collections" image:[UIImage imageNamed:@"collections.png"]], nil]];
+                            paperFoldNavController, nil]];
 	
 	[self addCenterButtonWithImage:[UIImage imageNamed:@"add-wine-button.png"] highlightImage:nil];
 }
