d01448de3108a31d00de7ab45615d9f0a33263a9
Christian Stangier <chris.stangier@gmail.com>
2013-09-27 20:36:58 +0200

    added a new table view controller

---
+++ Controllers/VYCountryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VYCountryTableViewController.h
+//  Vineyard
+//
+//  Created by Christian Stangier on 27.09.13.
+//  Copyright (c) 2013 Christian Stangier. All rights reserved.
+//
+
+#import "VYAbstractTableViewController.h"
+
+@interface VYCountryTableViewController : VYAbstractTableViewController
+
+@end
---
+++ Controllers/VYCountryTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  VYCountryTableViewController.m
+//  Vineyard
+//
+//  Created by Christian Stangier on 27.09.13.
+//  Copyright (c) 2013 Christian Stangier. All rights reserved.
+//
+
+#import "VYCountryTableViewController.h"
+
+@interface VYCountryTableViewController ()
+
+@end
+
+@implementation VYCountryTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
